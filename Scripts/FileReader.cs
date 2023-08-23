07f5b51d55d5a456a24f20949c73e1b0182bfcf7
Viljo Kirsilä <vkirsilaa@gmail.com>
2023-08-23 10:00:56 +0300

    Add project files.

---
+++ Scripts/FileReader.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakeWriter
+{
+    internal class FileReader
+    {
+
+        public string ReadAllText(string filePath)
+        {
+            try
+            {
+                Console.WriteLine("Reading the file...");
+                Thread.Sleep(1000);
+                return File.ReadAllText(filePath);
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("File not found.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred: " + ex.Message);
+            }
+
+            return string.Empty;
+        }
+    }
+}
---
+++ Scripts/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.Text;
+using FakeWriter;
+
+class Program
+{
+    static void Main()
+    {
+        bool login = false;
+
+        Console.OutputEncoding = Console.InputEncoding = Encoding.Unicode;
+
+        Console.WriteLine("Enter password to use this software");
+
+        while (login == false)
+        {
+           
+            string userInput = Console.ReadLine();
+
+            if (userInput.Equals("Rakis24"))
+            {
+                login = true;
+                Console.WriteLine("Success!");
+                StartWriting();
+            }
+            else
+            {
+                Console.WriteLine("Wrong password! Try again.");
+            }
+        }
+    }
+
+    static void StartWriting()
+    {
+
+        Console.WriteLine("You need to give the path of your text file! (C:/somepath/somefolder)");
+
+        string filePath = Console.ReadLine();
+
+        FileReader fileReader = new FileReader();
+        string fileContent = fileReader.ReadAllText(filePath);
+
+        string[] words = fileContent.Split(new[] { ' ', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+
+        if(words.Length == 0)
+        {
+            Console.WriteLine("File does not exist or is empty!");
+            return;
+        }
+
+        Console.Clear();
+
+        Console.WriteLine("Press any key to display the words. Press 'q' to quit.");
+
+        int currentIndex = 0;
+
+        while (currentIndex < words.Length)
+        {
+            ConsoleKeyInfo keyInfo = Console.ReadKey();
+            if (keyInfo.KeyChar == 'q')
+            {
+                Console.WriteLine("\nQuitting...");
+            }
+            else
+            {
+                Console.Write("\b \b");
+                Console.Write(words[currentIndex] + " ");
+                currentIndex++;
+            }
+        }
+        Console.WriteLine("\nReached the end! Closing in 3 seconds!");
+        Thread.Sleep(3000);
+    }
+}
